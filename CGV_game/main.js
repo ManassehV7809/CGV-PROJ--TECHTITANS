c932536244c2efdb40a93a0294d91cf22b914fd9
ManassehV7809 <104124845+ManassehV7809@users.noreply.github.com>
2023-09-22 16:33:02 +0200

    added stuff
    Initialization: Set the camera above the maze for a bird's-eye view and adjusted the aspect ratio according to the window dimensions.
    
    Ground: Added a texture to the ground plane and rotated it to align horizontally.
    
    Lighting: Implemented both point and ambient lighting, added a helper to visualize the point light's position.
    
    Background: Integrated a space-themed background for a more immersive experience.
    
    Player: Defined the player as a red cube, placed at the top-left corner of the maze. Introduced a mouse-controlled camera rotation system that revolves around the player.
    
    Walls: Used an array to manage and detect collisions with maze walls. Integrated a texture for the walls to enhance their appearance.
    
    Maze Generation: Implemented a dynamic maze generation algorithm based on the Depth-First Search (DFS) method. Adjusted wall removal based on the algorithm's progress.
    
    Camera Movement: Set the camera to follow the player and adjust its position based on player movement.
    
    Controls: Introduced keyboard controls for player movement and mouse controls for camera rotation. Enhanced collision detection to ensure the player cannot move through walls.
    
    Rendering: Utilized requestAnimationFrame for smoother and efficient game rendering

--- CGV_game/main.js
+++ CGV_game/main.js
@@ -7,133 +7,258 @@ let keyboard = {};
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 camera.position.set(0, 15, 2);
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#game'),
-});
-
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#game') });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.set(0, 80, 0);  // Position the camera above
+camera.lookAt(0, 0, 0);  // Make it look downwards
 
 
+// Ground setup
 const ground = new THREE.Mesh(
-    new THREE.PlaneGeometry(200, 200, 100, 100),
+    new THREE.PlaneGeometry(500, 333, 72, 72),
     new THREE.MeshStandardMaterial({color: 0x00ff00})
 );
 ground.rotation.x = -Math.PI / 2;
 scene.add(ground);
 
-const pointLight = new THREE.PointLight(0xffffff, 5000);
-pointLight.position.set(10,50,10);
+const groundMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./images/ground texture -vusani.avif')
+});
+ground.material = groundMaterial;
+
+// Lighting setup
+const pointLight = new THREE.PointLight(0x7f7fff, 1000);
+pointLight.position.set(10, 50, 10);
 const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(pointLight, ambientLight);
-
 const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
+scene.add(lightHelper);
 
-//const controls = new OrbitControls(camera, renderer.domElement);
-
+// Background
 const spaceTexture = new THREE.TextureLoader().load('./images/space.jpg');
 scene.background = spaceTexture;
 
+// Player setup
+const cellSize = 25; // This means a 20x13 maze for a 500x333 ground.
+
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.BoxGeometry(5, 2, 3),
     new THREE.MeshStandardMaterial({color: 0xff0000})
 );
-cube.position.set(0, 1.5, 0);
-
+cube.position.set(-250 + cellSize / 2, 1.5, -166.5 + cellSize / 2);  // Start at the top-left corner of the maze
 scene.add(cube);
 
-
-function followPlayer() {
-    camera.position.x = cube.position.x;
-    camera.position.y = cube.position.y + 7;
-    camera.position.z = cube.position.z + 15;
-}
-
-//todo: add controls
-// Set initial velocity for player movement
-const playerVelocity = new THREE.Vector3();
-
-// Define movement speed and acceleration
-const movementSpeed = 0.5;
-const acceleration = 0.1;
-
-// Listen for keydown and keyup events to handle player movement
-document.addEventListener("keydown", (event) => {
-    keyboard[event.key] = true;
-    console.log(keyboard);
+// Walls setup
+const wallMaterial = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load('./images/wall texture too - vusani.avif')
 });
 
+let walls = []; // Array to store all wall meshes
+
+function addWall(x, y, z, width, height, depth) {
+    const wall = new THREE.Mesh(
+        new THREE.BoxGeometry(width, height, depth),
+        wallMaterial
+    );
+    wall.position.set(x, y, z);
+    walls.push(wall); // Save the wall for collision checks
+    scene.add(wall);
+}
+
+const wallHeight = 10;
+const wallThickness = 2;
+addWall(0, wallHeight / 2, -166.5 + wallThickness / 2, 500, wallHeight, wallThickness);
+addWall(0, wallHeight / 2, 166.5 - wallThickness / 2, 500, wallHeight, wallThickness);
+addWall(-250 + wallThickness / 2, wallHeight / 2, 0, wallThickness, wallHeight, 333);
+addWall(250 - wallThickness / 2, wallHeight / 2, 0, wallThickness, wallHeight, 333);
+
+
+
+// Maze Generation
+
+const rows = Math.floor(500 / cellSize);
+const cols = Math.floor(333 / cellSize);
+let stack = [];
+let grid = Array(rows).fill(null).map(() => Array(cols).fill(null));
+
+function Cell(i, j) {
+    this.i = i;
+    this.j = j;
+    this.visited = false;
+    this.walls = {
+        top: true,
+        right: true,
+        bottom: true,
+        left: true
+    };
+}
+
+function getUnvisitedNeighbors(cell) {
+    const neighbors = [];
+    const top = grid[cell.i][cell.j - 1];
+    const right = grid[cell.i + 1] ? grid[cell.i + 1][cell.j] : undefined;
+    const bottom = grid[cell.i][cell.j + 1];
+    const left = grid[cell.i - 1] ? grid[cell.i - 1][cell.j] : undefined;
+
+    if (top && !top.visited) neighbors.push(top);
+    if (right && !right.visited) neighbors.push(right);
+    if (bottom && !bottom.visited) neighbors.push(bottom);
+    if (left && !left.visited) neighbors.push(left);
+
+    if (neighbors.length) {
+        return neighbors[Math.floor(Math.random() * neighbors.length)];
+    }
+}
+
+function removeWalls(a, b) {
+    const x = a.i - b.i;
+    if (x === 1) {
+        a.walls.left = false;
+        b.walls.right = false;
+    } else if (x === -1) {
+        a.walls.right = false;
+        b.walls.left = false;
+    }
+
+    const y = a.j - b.j;
+    if (y === 1) {
+        a.walls.top = false;
+        b.walls.bottom = false;
+    } else if (y === -1) {
+        a.walls.bottom = false;
+        b.walls.top = false;
+    }
+}
+
+function generateMaze() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j] = new Cell(i, j);
+        }
+    }
+
+    let current = grid[0][0];
+    current.visited = true;
+    stack.push(current);
+
+    while (stack.length) {
+        const next = getUnvisitedNeighbors(current);
+
+        if (next) {
+            next.visited = true;
+
+            // Remove walls between current and next cells
+            removeWalls(current, next);
+
+            stack.push(next);
+            current = next;
+        } else {
+            current = stack.pop();
+        }
+    }
+}
+
+
+generateMaze();
+
+
+
+// Create Walls based on Maze Grid
+function drawMaze() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            const cell = grid[i][j];
+            const x = i * cellSize - 250 + cellSize / 2; // Offset by half the ground width
+            const z = j * cellSize - 166.5 + cellSize / 2; // Offset by half the ground height
+
+            if (cell.walls.top) {
+                addWall(x, wallHeight / 2, z - cellSize / 2, cellSize, wallHeight, wallThickness);
+            }
+            if (cell.walls.right) {
+                addWall(x + cellSize / 2, wallHeight / 2, z, wallThickness, wallHeight, cellSize);
+            }
+            if (cell.walls.bottom) {
+                addWall(x, wallHeight / 2, z + cellSize / 2, cellSize, wallHeight, wallThickness);
+            }
+            if (cell.walls.left) {
+                addWall(x - cellSize / 2, wallHeight / 2, z, wallThickness, wallHeight, cellSize);
+            }
+        }
+    }
+}
+
+drawMaze();
+
+
+
+
+
+// Camera follow player setup
+function followPlayer() {
+    camera.position.x = cube.position.x;
+    camera.position.y = cube.position.y + 15; // Make the camera higher for better visibility
+    camera.position.z = cube.position.z + 25;
+    camera.lookAt(cube.position);
+}
+// Player movement
+const playerVelocity = new THREE.Vector3();
+const movementSpeed = 0.7;
+const acceleration = 0.2;
+
+document.addEventListener("keydown", (event) => {
+    keyboard[event.key] = true;
+});
 document.addEventListener("keyup", (event) => {
     keyboard[event.key] = false;
 });
 
-// Define initial camera rotation angles
 let cameraYaw = 0;
 let cameraPitch = 0;
-
-// Define mouse sensitivity for camera control
 const mouseSensitivity = 0.002;
 
-// Listen for mouse movement to control the camera
 document.addEventListener("mousemove", (event) => {
     cameraYaw -= event.movementX * mouseSensitivity;
     cameraPitch -= event.movementY * mouseSensitivity;
-
-    // Limit camera pitch to avoid flipping
     cameraPitch = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, cameraPitch));
-
-    // Apply camera rotation to the player cube
     cube.rotation.set(0, cameraYaw, 0);
-
-    // Update camera position based on player cube's position
     camera.position.x = cube.position.x - 15 * Math.sin(cameraYaw);
     camera.position.y = cube.position.y + 7;
     camera.position.z = cube.position.z - 15 * Math.cos(cameraYaw);
-
-    // Point the camera at the player cube's position
-    const cameraLookAt = new THREE.Vector3(
-        cube.position.x,
-        cube.position.y + 5,
-        cube.position.z-20
-    );
+    const cameraLookAt = new THREE.Vector3(cube.position.x, cube.position.y + 5, cube.position.z-20);
     camera.lookAt(cameraLookAt);
 });
 
-function handlePlayerMovement() {
 
-    if (keyboard["w"]) {
-        // Move forward
-        playerVelocity.z -= acceleration;
-        console.log("W pressed");
-    }
-    if (keyboard["s"]) {
-        // Move backward
-        playerVelocity.z += acceleration;
-        console.log("S pressed");
-    }
-    if (keyboard["a"]) {
-        // Move left
-        playerVelocity.x -= acceleration;
-        console.log("A pressed");
-    }
-    if (keyboard["d"]) {
-        // Move right
-        playerVelocity.x += acceleration;
-        console.log("D pressed");
-    }
 
-        // Apply velocity to the player cube's position
-    cube.position.add(playerVelocity);
+function checkCollision(direction) {
+    const playerBox = new THREE.Box3().setFromObject(cube);
+    playerBox.expandByVector(new THREE.Vector3(0.5, 0.5, 0.5)); // Expand the bounding box a little for better feel
 
-    // Limit the player's maximum speed
-    playerVelocity.clampLength(0, movementSpeed);
+    const futureBox = playerBox.clone().translate(direction);
+
+    for (let wall of walls) {
+        const wallBox = new THREE.Box3().setFromObject(wall);
+        if (futureBox.intersectsBox(wallBox)) {
+            return true;
+        }
+    }
+    return false;
 }
 
+function handlePlayerMovement() {
+    const direction = new THREE.Vector3();
+
+    if (keyboard["w"]) direction.z = -acceleration;
+    if (keyboard["s"]) direction.z = acceleration;
+    if (keyboard["a"]) direction.x = -acceleration;
+    if (keyboard["d"]) direction.x = acceleration;
+
+    if (!checkCollision(direction)) {
+        cube.position.add(direction);
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -142,6 +267,4 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-
 animate();
-
