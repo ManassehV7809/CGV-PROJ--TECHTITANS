eb62cf712958112ea5aefaa90eec520ba09e6829
Arinaho Muleba <arinaomuleluz@gmail.com>
2023-09-26 20:02:08 +0200

    Added control menu, added some textures on lvl 1 & some UI enhancements

--- CGV_game/levels/level1.js
+++ CGV_game/levels/level1.js
@@ -5,16 +5,16 @@ import Ball from '../models/ball.js'
   //todo: define lights
 let lights = [];
     let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-    light.position.set(-100, 100, 100);
+    light.position.set(-100, 200, 100);
     light.target.position.set(0, 0, 0);
     light.castShadow = true;
     light.shadow.bias = -0.001;
-    light.shadow.mapSize.width = 4096;
-    light.shadow.mapSize.height = 4096;
+    light.shadow.mapSize.width = 10000;
+    light.shadow.mapSize.height = 10000;
     light.shadow.camera.near = 0.1;
     light.shadow.camera.far = 500.0;
     light.shadow.camera.near = 0.5;
-    light.shadow.camera.far = 500.0;
+    light.shadow.camera.far = 1000.0;
     light.shadow.camera.left = 50;
     light.shadow.camera.right = -50;
     light.shadow.camera.top = 50;
@@ -22,7 +22,7 @@ let lights = [];
 
     lights.push(light);
 
-    light = new THREE.AmbientLight(0x00FF00, 0.25);
+    light = new THREE.AmbientLight(0xffffff, 0.25);
     lights.push(light);
 
     //todo: define background
@@ -32,41 +32,38 @@ let lights = [];
     const plane = new THREE.Mesh(
         new THREE.PlaneGeometry(1000, 1000, 10, 10),
         new THREE.MeshStandardMaterial({
-            color: 0xff0000,
+
+
           }));
 
     plane.castShadow = false;
     plane.receiveShadow = true;
     plane.rotation.x = -Math.PI / 2;
 
+    const planeTexture = new THREE.TextureLoader().load('../textures/concrete.jpg');
+    plane.material.map = planeTexture;
+
     //todo: define objects
 
     let objects = [];
-    //defina wall
+    //define wall
     const wall = new THREE.Mesh(
         new THREE.BoxGeometry(100, 100, 10),
         new THREE.MeshStandardMaterial({
-            color: 0xff0000,
             }));
     wall.position.set(0, 50, -50);
     wall.castShadow = true;
     wall.receiveShadow = true;
+
+    //load texture
+    const wallTexture = new THREE.TextureLoader().load('../textures/brick_wall.jpg');
+    wall.material.map = wallTexture;
+
     objects.push(wall);
 
     objects.push(Ball);
 
-    //define ball
-    // const ball = new THREE.Mesh(
-    //     new THREE.SphereGeometry(5, 32, 32),
-    //     new THREE.MeshStandardMaterial({
-    //         color: 0xff0000,
-    //         }));
-    // ball.position.set(10, 5, 10);
-    // ball.castShadow = true;
-    // ball.receiveShadow = true;
 
-
-    // objects.push(ball);
 
 
     let Level1 = new Level(lights, bg, plane, objects);
